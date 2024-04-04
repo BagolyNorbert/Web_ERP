4ddaa0c65f79f73f00827556ff4ba03781674a4c
BagolyNorbert <162550217+BagolyNorbert@users.noreply.github.com>
2024-04-04 18:46:36 +0300

    Module created
    Created my module with every basic settings

---
+++ custom-addons/Web_Erp_module/__manifest__.py
@@ -0,0 +1,15 @@
+{
+    'name': 'Web_ERP_module',
+    'version': '1.0',
+    'description': """
+This is my web application for manageing worklogs, employees, etc.
+""",
+    'author': 'Bagoly Norbert',
+    'data': [],
+    'test': [],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'license': 'LGPL-3',
+}
